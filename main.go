1e71119c3e7d75b61b5fa44cbbe692b06265c4a9
Dirk Arnez <smalldirkalex@gmail.com>
2026-07-08 16:39:41 +0800

    - update main.go

--- main.go
+++ main.go
@@ -9,17 +9,19 @@ import (
 )
 
 var (
-	vid   *string
-	pid   *string
-	serialNumber *string
+	vid   string
+	pid   string
+	serialNumber string
 	isUSB bool
+	list bool
 )
 
 func main() {
-	flag.StringVar(vid, "vid", "", "VID, default null")
-	flag.StringVar(pid, "pid", "", "PID, default null")
-	flag.StringVar(serialNumber, "serialnumber", "", "Serialnumber, default null")
+	flag.StringVar(&vid, "vid", "", "VID, default null")
+	flag.StringVar(&pid, "pid", "", "PID, default null")
+	flag.StringVar(&serialNumber, "serialnumber", "", "Serial Number, default null")
 	flag.BoolVar(&isUSB, "usb", true, "USB. Default true")
+	flag.BoolVar(&list, "list", false, "List Ports. Default false")
 	flag.Parse()
 
 	// if isUSB && (vid == nil || len(*vid) < 1 || pid == nil || len(*pid) < 1) {
@@ -30,14 +32,27 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	
 	if len(ports) == 0 {
 		log.Fatal("No serial ports found!")
 		return
 	}
 
-	for _, port := range ports {
-		if port.IsUSB == isUSB && ( vid != nil || port.VID == *vid ) && ( pid != nil || port.PID == *pid ) && ( serialNumber != nil || port.SerialNumber == *serialNumber )  {
-			fmt.Print(port.Name)
+	if list {
+		for _, port := range ports {
+			fmt.Printf("Found port: %s\n", port.Name)
+			if port.IsUSB {
+				fmt.Printf("   USB VID    %s\n", port.VID)
+				fmt.Printf("   USB PID    %s\n", port.PID)
+				fmt.Printf("   USB prod.  %s\n", port.Product)
+				fmt.Printf("   USB serial %s\n", port.SerialNumber)
+			}
+		}
+	} else {
+		for _, port := range ports {
+			if port.IsUSB == isUSB && ( vid != "" || port.VID == vid ) && ( pid != "" || port.PID == pid ) && ( serialNumber != "" || port.SerialNumber == *serialNumber )  {
+				fmt.Print(port.Name)
+			}
 		}
 	}
 }
